c15affb30083bceb240768c8eabc1702f3dfee1b
joshmorenx <joshuamoreno21@gmail.com>
2024-05-17 16:19:19 -0600

    added api for the backend

--- server/app.js
+++ server/app.js
@@ -13,6 +13,11 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Backend connected to frontend" })
 })
 
+app.post("/", async (req, res) => {
+    const { message } = req.body
+    res.json({ returningMsg : message })
+})
+
 app.listen(8080, () => {
     console.log("server is running on port 8080")
 })
