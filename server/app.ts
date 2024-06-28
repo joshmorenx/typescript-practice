04242f1fb2a37e91556dec350c1e9652646742f3
joshmorenx <joshuamoreno21@gmail.com>
2024-06-28 10:47:38 -0600

    migrated server instance from js to ts

---
+++ server/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+const allowedOrigins: string[] = ['http://localhost:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:5173']
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: allowedOrigins }));
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Backend connected to frontend" })
+})
+
+app.post("/", async (req, res) => {
+    const { message } = req.body
+    res.json({ returningMsg : message })
+})
+
+app.listen(8080, () => {
+    console.log("server is running on port 8080")
+})
